549169e0a12a456c8ef65c0eb6ce1e7a44328a28
slydragonn <alogo18m@gmail.com>
2025-01-02 20:25:30 -0500

    feat(client): passwords data list

--- client/src/pages/Passwords.tsx
+++ client/src/pages/Passwords.tsx
@@ -1,18 +1,28 @@
 import DesktopLayout from '@/layouts/Desktop'
 import useAuthStore from '@/store/authStore'
 import usePasswordStore from '@/store/passwordStore'
+import { PasswordData } from '@/types/passwords'
 import { defaultDateFormat } from '@/utils/date-format'
 import {
+  CopyIcon,
+  EyeClosedIcon,
+  EyeOpenIcon,
   InfoCircledIcon,
   MagnifyingGlassIcon,
+  Pencil1Icon,
   PlusIcon,
+  ResetIcon,
+  StarFilledIcon,
   StarIcon,
+  TrashIcon,
 } from '@radix-ui/react-icons'
 import {
   Badge,
   Box,
   Callout,
   Card,
+  Code,
+  DataList,
   Flex,
   IconButton,
   ScrollArea,
@@ -20,16 +30,44 @@ import {
   Text,
   TextField,
   Tooltip,
+  Link,
+  Blockquote,
+  DropdownMenu,
+  Button,
 } from '@radix-ui/themes'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import toast, { Toaster } from 'react-hot-toast'
 
 export default function Passwords() {
   const { passwords, getData, loading } = usePasswordStore()
   const { token } = useAuthStore()
+  const [showPassword, setShowPassword] = useState<PasswordData | null>()
+  const [isSecret, setIsSecret] = useState<boolean>(true)
 
   useEffect(() => {
     getData('/server/api/passwords', token)
   }, [getData, token])
+
+  const findPasswordByID = (id: string) => {
+    setIsSecret(() => true)
+
+    const password = passwords.find((el) => el.ID == id)
+
+    if (password) {
+      setShowPassword(() => password)
+    }
+  }
+
+  const handleCopy = (password: string) => {
+    navigator.clipboard
+      .writeText(password)
+      .then(() => {
+        toast.success('Password Copied')
+      })
+      .catch((error) => {
+        toast.error(error)
+      })
+  }
 
   return (
     <DesktopLayout>
@@ -73,6 +111,7 @@ export default function Passwords() {
                               overflow: 'hidden',
                               whiteSpace: 'nowrap',
                             }}
+                            onClick={() => findPasswordByID(password.ID)}
                           >
                             {password.Name}
                           </Text>
@@ -100,6 +139,122 @@ export default function Passwords() {
           </Box>
         )}
       </Box>
+      <Box
+        width="100%"
+        height="100vh"
+        p="6"
+        style={{ background: 'var(--indigo-3)' }}
+      >
+        {showPassword ? (
+          <Flex direction="column" gap="6">
+            <Card variant="ghost">
+              <Flex gap="2" justify="end">
+                <Tooltip content="Favorite">
+                  <IconButton variant="surface">
+                    {showPassword?.Favorite ? <StarFilledIcon /> : <StarIcon />}
+                  </IconButton>
+                </Tooltip>
+                <DropdownMenu.Root>
+                  <DropdownMenu.Trigger>
+                    <Button variant="solid" size="2">
+                      Options
+                      <DropdownMenu.TriggerIcon />
+                    </Button>
+                  </DropdownMenu.Trigger>
+                  <DropdownMenu.Content size="2" variant="soft">
+                    <DropdownMenu.Item onClick={() => setShowPassword(null)}>
+                      Quit <ResetIcon />
+                    </DropdownMenu.Item>
+                    <DropdownMenu.Item>
+                      Edit <Pencil1Icon />
+                    </DropdownMenu.Item>
+                    <DropdownMenu.Separator />
+                    <DropdownMenu.Item color="red">
+                      Delete <TrashIcon />
+                    </DropdownMenu.Item>
+                  </DropdownMenu.Content>
+                </DropdownMenu.Root>
+              </Flex>
+            </Card>
+            <Card variant="classic">
+              <DataList.Root size="3">
+                <DataList.Item>
+                  <DataList.Label>Name</DataList.Label>
+                  <DataList.Value>{showPassword?.Name}</DataList.Value>
+                </DataList.Item>
+                <DataList.Item>
+                  <DataList.Label>Created at</DataList.Label>
+                  <DataList.Value>
+                    {defaultDateFormat(showPassword?.CreatedAt || '')}
+                  </DataList.Value>
+                </DataList.Item>
+                <DataList.Item>
+                  <DataList.Label>Updated at</DataList.Label>
+                  <DataList.Value>
+                    {defaultDateFormat(showPassword?.UpdatedAt || '')}
+                  </DataList.Value>
+                </DataList.Item>
+                <DataList.Item>
+                  <DataList.Label>Username</DataList.Label>
+                  <DataList.Value>{showPassword?.Username}</DataList.Value>
+                </DataList.Item>
+                <DataList.Item>
+                  <DataList.Label>Password</DataList.Label>
+                  <DataList.Value>
+                    <Flex align="center" gap="2">
+                      <Code variant="ghost" size="3">
+                        {isSecret
+                          ? showPassword?.Password.replace(/./g, '*')
+                          : showPassword?.Password}
+                      </Code>
+                      <IconButton
+                        size="1"
+                        aria-label="copy value"
+                        color="gray"
+                        variant="ghost"
+                        onClick={() => handleCopy(showPassword?.Password)}
+                      >
+                        <CopyIcon />
+                      </IconButton>
+                      <IconButton
+                        size="1"
+                        aria-label="see value"
+                        color="gray"
+                        variant="ghost"
+                        onClick={() => setIsSecret((is) => !is)}
+                      >
+                        {isSecret ? <EyeOpenIcon /> : <EyeClosedIcon />}
+                      </IconButton>
+                    </Flex>
+                  </DataList.Value>
+                </DataList.Item>
+                <DataList.Item>
+                  <DataList.Label>Website</DataList.Label>
+                  <DataList.Value>
+                    <Link target="_blank" href={showPassword?.Website}>
+                      {showPassword?.Website}
+                    </Link>
+                  </DataList.Value>
+                </DataList.Item>
+                <DataList.Item>
+                  <DataList.Label>Note</DataList.Label>
+                  <DataList.Value>
+                    <Blockquote color="gray">{showPassword?.Note}</Blockquote>
+                  </DataList.Value>
+                </DataList.Item>
+              </DataList.Root>
+            </Card>
+          </Flex>
+        ) : (
+          <Callout.Root variant="outline">
+            <Callout.Icon>
+              <InfoCircledIcon />
+            </Callout.Icon>
+            <Callout.Text>Select a Password Card</Callout.Text>
+          </Callout.Root>
+        )}
+      </Box>
+      <Toaster position="bottom-right" />
     </DesktopLayout>
   )
 }
